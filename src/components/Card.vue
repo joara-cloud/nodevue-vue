b5487158866767b252182f60a56f766890d8c53f
ara <joarario92@gmail.com>
2020-10-07 02:05:59 +0900

    Pratice loading

---
+++ src/components/Card.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div v-if="loading">loading card...</div>
+    <div v-else>Card id :  {{this.cid }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cid: 0,
+      loading: false
+    }
+  },
+  watch: {
+    // '$route'() {
+    //   this.cid = this.$route.params.cid;
+    // }
+    '$route': {
+      handler: 'fetchData',
+      immediate: true // 핸들러에 등록된 함수가 즉시 실행되므로 따로 fetchData() 이런식으로 호출하지 않아도됨
+    }
+  },
+  // created() {
+  //   this.fetchData();
+  // },
+  methods: {
+    fetchData() {
+      this.loading = true;
+      setTimeout(() => {
+        this.cid = this.$route.params.cid;
+        this.loading = false;
+      }, 500)
+    }
+  }
+}
+</script>
---
+++ src/views/UserList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, i) in userlist" v-bind:key="i">
+        <p>user id : {{item.userid}}</p>
+        <p>user name : {{item.name}}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        id: '',
+        name: ''
+      },
+      userlist: []
+    }
+  },
+  created() {
+    this.$http.post('/users/userlist')
+      .then(response => {
+        this.userlist = response.data;
+      })
+      .catch(err => {
+        console.log(err);
+        return;
+      })
+  }
+}
+</script>
+
+<style>
+
+</style>
